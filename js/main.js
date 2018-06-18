c5e879ab9b1217bfa8e9289029a11cfc3485d0cb
jainatee <jbteeluck@gmail.com>
2018-06-17 20:43:48 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -12,20 +12,33 @@ var cardFour = 'king';
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
+var checkForMatch = function() {
+	if (cardsInPlay.length == 2 && cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+};
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var flipCard = function(cardId) {
+	var checkForMatch;
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
 
-if (cardsInPlay.length == 2 && cardsInPlay[0] === cardsInPlay[1]) {
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
+/*
+	if (cardsInPlay.length == 2 && cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
-} else {
+} 	else {
 	alert("Sorry, try again.");
-}
-
+};
+*/
 
 
